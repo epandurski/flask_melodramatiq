795db5bc3f8089871b1de827c82da0ae3ceefaba
Evgeni Pandurski <epandurski@gmail.com>
2019-03-31 22:29:09 +0300

    Add tests

--- tests/test_rabbitmq_broker.py
+++ tests/test_rabbitmq_broker.py
@@ -2,6 +2,19 @@ import dramatiq
 import time
 
 
+def test_broker_connection_attrs(app, rabbitmq_broker):
+    rabbitmq_broker.init_app(app)
+    assert hasattr(rabbitmq_broker, 'channel')
+    assert hasattr(rabbitmq_broker, 'connection')
+    del rabbitmq_broker.channel
+    del rabbitmq_broker.connection
+
+
+def test_connection_closed_error():
+    e = Exception()
+    assert isinstance(dramatiq.ConnectionClosed(e), Exception)
+
+
 def test_publish_message(app, rabbitmq_broker, run_mock):
     @rabbitmq_broker.actor
     def task():
