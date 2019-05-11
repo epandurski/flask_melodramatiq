a8d35ab96b086bab7ca78d97afdac796f07adb3c
Evgeni Pandurski <epandurski@gmail.com>
2019-05-11 13:49:08 +0300

    Add a routing_key argument to RabbitmqBrokerMixin.publish_message()

--- flask_melodramatiq/rabbitmq.py
+++ flask_melodramatiq/rabbitmq.py
@@ -2,27 +2,30 @@ import dramatiq
 
 
 class RabbitmqBrokerMixin:
-    def publish_message(self, message, *, exchange=''):
+    def publish_message(self, message, *, exchange='', routing_key=None):
         """Publish a message on an exchange.
 
-        :param message: The message. The routing key will be
-          ``f"dramatiq.events.{message.actor_name}"`` if
-          ``message.queue_name`` is `None`, and ``message.queue_name``
-          otherwise.
-
+        :param message: The message
         :type message: dramatiq.Message
 
         :param exchange: The name of the RabbitMQ exchange on which to
           publish the message
 
+        :param routing_key: The message routing key. If the value is
+          `None`, the routing key will be
+          ``f"dramatiq.events.{message.actor_name}"`` if
+          ``message.queue_name`` is `None`, and ``message.queue_name``
+          otherwise.
+
         """
 
         import pika
 
-        if message.queue_name is None:
-            routing_key = 'dramatiq.events.' + message.actor_name
-        else:
-            routing_key = message.queue_name
+        if routing_key is None:
+            if message.queue_name is None:
+                routing_key = 'dramatiq.events.' + message.actor_name
+            else:
+                routing_key = message.queue_name
 
         properties = pika.BasicProperties(
             delivery_mode=2,
